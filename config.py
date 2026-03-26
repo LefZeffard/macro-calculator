b23429946a3ff663d139e2a83941a82e6f93a25f
unknown <edwardliu614@gmail.com>
2026-03-26 18:59:17 -0400

    Connect app to neon

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-only-fallback'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
